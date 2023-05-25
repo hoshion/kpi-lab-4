488ea1c89f540bdf4c0143433fda68061ead9724
andrewvitrenko <avitrenko2004@gmail.com>
2023-05-26 00:07:10 +0200

    feat: added integration tests for `balancer`

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -19,14 +19,51 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+	// test server1
+	server1, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
 	if err != nil {
 		t.Error(err)
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	server1Header := server1.Header.Get("lb-from")
+	if server1Header != "server1:8080" {
+		t.Errorf("Wrong server for server1 - %s", server1Header)
+	}
+
+	// test server2
+	server2, err := client.Get(fmt.Sprintf("%s/check4", baseAddress))
+	if err != nil {
+		t.Error(err)
+	}
+
+	server2Header := server2.Header.Get("lb-from")
+	if server2Header != "server2:8080" {
+		t.Errorf("Wrong server for server 2 - %s", server2Header)
+	}
+
+	// test server3
+	server3, err := client.Get(fmt.Sprintf("%s/check2", baseAddress))
+	if err != nil {
+		t.Error(err)
+	}
+
+	server3Header := server3.Header.Get("lb-from")
+	if server3Header != "server3:8080" {
+		t.Errorf("Wrong server for server3 - %s", server3Header)
+	}
+
+	// test repeated request
+	server1Repeat, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
+	if err != nil {
+		t.Error(err)
+	}
+
+	server1RepeatHeader := server1Repeat.Header.Get("lb-from")
+	if server1Header != server1RepeatHeader {
+		t.Errorf("Headers are not equal. origin - %s, repeat - %s", server1Header, server1RepeatHeader)
+	}
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальника.
 }

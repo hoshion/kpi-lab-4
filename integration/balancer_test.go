e7ad6d2d08c13c0ff46b9a9309afe7408b527d34
andrewvitrenko <avitrenko2004@gmail.com>
2023-05-27 15:16:50 +0200

    fix: rewrote tests with `gocheck`

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -6,7 +6,15 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	. "gopkg.in/check.v1"
 )
+
+func Test(t *testing.T) { TestingT(t) }
+
+type IntegrationSuite struct{}
+
+var _ = Suite(&IntegrationSuite{})
 
 const baseAddress = "http://balancer:8090"
 
@@ -14,65 +22,57 @@ var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
+func (s *IntegrationSuite) TestBalancer(c *C) {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
+		c.Skip("Integration test is not enabled")
 	}
 
 	// test server1
 	server1, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
 	if err != nil {
-		t.Error(err)
+		c.Error(err)
 	}
 
 	server1Header := server1.Header.Get("lb-from")
-	if server1Header != "server1:8080" {
-		t.Errorf("Wrong server for server1 - %s", server1Header)
-	}
+	c.Check(server1Header, Equals, "server1:8080")
 
 	// test server2
 	server2, err := client.Get(fmt.Sprintf("%s/check4", baseAddress))
 	if err != nil {
-		t.Error(err)
+		c.Error(err)
 	}
 
 	server2Header := server2.Header.Get("lb-from")
-	if server2Header != "server2:8080" {
-		t.Errorf("Wrong server for server 2 - %s", server2Header)
-	}
+	c.Check(server2Header, Equals, "server2:8080")
 
 	// test server3
 	server3, err := client.Get(fmt.Sprintf("%s/check2", baseAddress))
 	if err != nil {
-		t.Error(err)
+		c.Error(err)
 	}
 
 	server3Header := server3.Header.Get("lb-from")
-	if server3Header != "server3:8080" {
-		t.Errorf("Wrong server for server3 - %s", server3Header)
-	}
+	c.Check(server3Header, Equals, "server3:8080")
 
 	// test repeated request
 	server1Repeat, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
 	if err != nil {
-		t.Error(err)
+		c.Error(err)
 	}
 
 	server1RepeatHeader := server1Repeat.Header.Get("lb-from")
-	if server1Header != server1RepeatHeader {
-		t.Errorf("Headers are not equal. origin - %s, repeat - %s", server1Header, server1RepeatHeader)
-	}
+	c.Check(server1RepeatHeader, Equals, server1Header)
 }
 
-func BenchmarkBalancer(b *testing.B) {
+func (s *IntegrationSuite) BenchmarkBalancer(c *C) {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		b.Skip("Integration test is not enabled")
+		c.Skip("Integration test is not enabled")
 	}
 
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < c.N; i++ {
 		_, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
 		if err != nil {
-			b.Error(err)
+			c.Error(err)
 		}
 	}
 }
